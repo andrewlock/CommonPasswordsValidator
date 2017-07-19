102275a50d3f71f6963ee1202c883bdab7ab8774
Andrew Lock <andrewlock.net@gmail.com>
2017-07-19 18:39:01 +0100

    Updated readme and log number of passwords loaded

--- src/CommonPasswordsValidator/Internal/PasswordLists.cs
+++ src/CommonPasswordsValidator/Internal/PasswordLists.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 namespace CommonPasswordsValidator.Internal
@@ -12,10 +13,12 @@ namespace CommonPasswordsValidator.Internal
         private const string Prefix = nameof(CommonPasswordsValidator) + ".PasswordLists.";
 
         private readonly int _requiredLength;
+        private readonly ILogger<PasswordLists> _logger;
 
-        public PasswordLists(IOptions<IdentityOptions> options)
+        public PasswordLists(IOptions<IdentityOptions> options, ILoggerFactory loggerFactory)
         {
             _requiredLength = options.Value.Password.RequiredLength;
+            _logger = loggerFactory.CreateLogger<PasswordLists>();
             Top100Passwords = new Lazy<HashSet<string>>(() => LoadPasswordList("10_million_password_list_top_100.txt"));
             Top500Passwords = new Lazy<HashSet<string>>(() => LoadPasswordList("10_million_password_list_top_500.txt"));
             Top1000Passwords = new Lazy<HashSet<string>>(() => LoadPasswordList("10_million_password_list_top_1000.txt"));
@@ -43,6 +46,8 @@ namespace CommonPasswordsValidator.Internal
                         StringComparer.OrdinalIgnoreCase);
                 }
             }
+
+            _logger.LogDebug("Loaded {NumberCommonPasswords} common passwords from resource {ResourceName}", hashset.Count, listName);
             return hashset;
         }
 
